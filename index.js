c233ed59fc340494809162fff4434ebacbfc0c16
Billy Rennekamp <billy.rennekamp@gmail.com>
2023-11-19 08:55:42 +0300

    add linea

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const RacerABI = require("./contractMetadata/ABI-chiliz-Racer.json");
 const Racer = require("./contractMetadata/chiliz-Racer.json"); // TODO: replace with mainnet
 const RacerChiliz = require("./contractMetadata/chiliz-Racer.json");
 const RacerLocalhost = require("./contractMetadata/localhost-Racer.json");
-
+const RacerLinea = require("./contractMetadata/linea-Racer.json");
 module.exports = {
   Racer: {
     abi: RacerABI.abi,
@@ -14,7 +14,9 @@ module.exports = {
       '88882': RacerChiliz,
       'chiliz': RacerChiliz,
       '1337': RacerLocalhost,
-      'localhost': RacerLocalhost
+      'localhost': RacerLocalhost,
+      '59140': RacerLinea,
+      'linea': RacerLinea,
     },
   }
 }
